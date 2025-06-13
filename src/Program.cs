6c02c9c65df9799c35c56c143a1aca1dec1b371d
Brendan Rae <brae23hockey@gmail.com>
2025-06-13 10:55:10 -0700

    Removing swagger auth button

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,5 @@
-using FluentValidation;
 using HabitTracker.Api.Infrastructure;
 using HabitTracker.Api.Infrastructure.Entities;
-using HabitTracker.Api.Validators;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
@@ -13,40 +10,7 @@ builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-    options =>
-    {
-        options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-        {
-            Title = "Habit Tracker API",
-            Version = "v1",
-            Description = "API for Habit Tracker application"
-        });
-        options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-        {
-            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-            Description = "Please enter a valid token",
-            Name = "Authorization",
-            Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
-            BearerFormat = "JWT",
-            Scheme = JwtBearerDefaults.AuthenticationScheme
-        });
-
-        options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-        {
-            {
-                new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-                {
-                    Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                    {
-                        Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                new string[] {}
-            }
-        });
-    });
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<HabitTrackerDbContext>();
 builder.Services.AddDistributedMemoryCache();
